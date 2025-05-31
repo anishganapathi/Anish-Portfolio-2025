0fbfe87bface300725d58cbc1694ebd713fbc2e6
Anish <104456169+anishganapathi@users.noreply.github.com>
2025-05-31 11:42:52 +0530

    + Css Changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Nav from "@/components/nav";
 import Hero from "@/components/hero";
 import AboutScroll from "@/components/AboutScroll";
 
+
+
 export default function Home() {
   return (
     <div className="">
